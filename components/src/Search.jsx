99e05e9accfb2c8a2031ab565f20fd9fbc27b037
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2023-05-01 16:09:00 +0100

    updated search.jsx

--- components/src/Search.jsx
+++ components/src/Search.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+ import {useState}from 'react'
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+
+   const handleInputChange = (event) => {
+     setSearchTerm(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setSearchResult(event.target.value);
+    }
+  };
   return (
     <div className='search'>
-        <input type="search"  id="search" />
+           <input
+        type="text"
+        placeholder="Search..."
+       value={searchTerm}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+      {<p style={{color:'white'}}>{searchResult}</p>}
     </div>
-  )
-}
+  );
+ }
+
+
